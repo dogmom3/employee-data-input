1773babcec403c86553aa8c900903bf840f164f8
dogmom3 <blair.seivers@yahoo.com>
2022-03-14 12:33:18 -0400

    data display & css framework

--- src/generatePage.js
+++ src/generatePage.js
@@ -5,46 +5,94 @@ const Intern = require("../lib/Intern")
 const employees = [];
 
 function generateManagerData(employees) {
+    let managers = employees.filter((employee)=> employee.getRole()==="Manager")
     let managerData = ''
-    for (let index = 0; index < employees.length; index++) {
+    for (let index = 0; index < managers.length; index++) {
         managerData += `
-    <ul id="data">
-    <h2 id="name">Name: ${employees[index].getName()}</h2>
-    <li id="list">Role: ${employees[index].getRole()}</li>
-    <li id="list">ID: ${employees[index].getId()}</li>
-    <li id="list">Email: <a href="mailto: " target="_blank"> ${employees[index].getEmail()}</a></li>
-    <li id="list">Office Number: ${employees[index].getOfficeNumber()}</li>
-</ul>`
+   
+<div class="card">
+  <div class="card-content">
+  <p class="title">${managers[index].getName()}
+    </p>
+    <p class="subtitle">${managers[index].getRole()}
+    </p>
+    <p class="subtitle">${managers[index].getId()}
+    </p>
+    <p class="subtitle">
+    ${managers[index].getOfficeNumber()}
+    </p>
+  </div>
+  <footer class="card-footer">
+    <p class="card-footer-item">
+      <span>
+      Email: <a href="mailto: " target="_blank"> ${managers[index].getEmail()}</a>
+      </span>
+    </p>
+  </footer>
+</div>
+`
+
     }
     return managerData
 };
 
 function generateEngineerData(employees) {
+    let engineers = employees.filter((employee)=> employee.getRole()==="Engineer")
     let engineerData = ''
-    for (let index = 1; index < employees.length; index++) {
+    for (let index = 0; index < engineers.length; index++) {
         engineerData += `
-        <ul id="data">
-        <h2 id="name">Name: ${employees[index].getName()} </h2>
-        <li id="list">Role: ${employees[index].getRole()}</li>
-        <li id="list">ID: ${employees[index].getId()} </li>
-        <li id="list">Email: <a href="mailto: " target="_blank"> ${employees[index].getEmail()}</a></li>
-        <li id="list">GitHub: ${employees[index].getGitHub()}</li>
-    </ul>`
+          <div class="card">
+  <div class="card-content">
+  <p class="title">${engineers[index].getName()}
+    </p>
+    <p class="subtitle">${engineers[index].getRole()}
+    </p>
+    <p class="subtitle">${engineers[index].getId()}
+    </p>
+    <p class="subtitle">
+    <span>
+    GitHub: <a href="https://github.com/${engineers[index].getGitHub()}" target="_blank"> ${engineers[index].getGitHub()}</a>
+    </span>
+  </p>
+  </div>
+  <footer class="card-footer">
+    <p class="card-footer-item">
+      <span>
+      Email: <a href="mailto: " target="_blank"> ${engineers[index].getEmail()}</a>
+      </span>
+    </p>
+  </footer>
+</div>
+    `
     }
     return engineerData
 };
 
 function generateInternData(employees) {
+    let interns = employees.filter((employee)=> employee.getRole()==="Intern")
     let internData = ''
-    for (let index = 1; index < employees.length; index++) {
+    for (let index = 0; index < interns.length; index++) {
     internData += `
-    <ul id="data">
-        <h2 id="name">Name: ${employees[index].getName()}</h2>
-        <li id="list">Role: ${employees[index].getRole()}</li>
-        <li id="list">ID: ${employees.getId()}</li>
-        <li id="list">Email: <a href="mailto: " target="_blank"> ${employees[index].getEmail()}</a></li>
-        <li id="list">School: ${employees[index].getSchool()}</li>
-    </ul>`
+    <div class="card">
+  <div class="card-content">
+  <p class="title">${interns[index].getName()}
+    </p>
+    <p class="subtitle">${interns[index].getRole()}
+    </p>
+    <p class="subtitle">${interns[index].getId()}
+    </p>
+    <p class="subtitle">${interns[index].getSchool()}
+  </p>
+  </div>
+  <footer class="card-footer">
+    <p class="card-footer-item">
+      <span>
+      Email: <a href="mailto: " target="_blank"> ${interns[index].getEmail()}</a>
+      </span>
+    </p>
+  </footer>
+</div>
+    `
 }
 return internData
     };
@@ -58,18 +106,25 @@ function generatePage(answers) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
         <title>Document</title>
     </head>
     <body>
-        <header>
-            <h1>MY TEAM</h1>
-        </header>
+    <header>
+    <section class="hero is-primary">
+        <div class="hero-body">
+          <p class="title has-text-centered">
+            My Team
+          </p>
+
+        </div>
+      </section>
+</header>
         <div id="container">
-            <div id="employee-data">
-                <div> ${generateManagerData(answers)} </div>
-                <div> ${generateEngineerData(answers)} </div>
-                <div> ${generateInternData(answers)} </div>
+            <div id="employee-data" class="is-flex is-flex-wrap-wrap is-justify-content-space-evenly">
+                 ${generateManagerData(answers)} 
+                 ${generateEngineerData(answers)} 
+                 ${generateInternData(answers)} 
             </div>
         </div>
     </body>
